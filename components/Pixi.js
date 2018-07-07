1a45bd7c32dd5ec9f85fdad88074d4bb5fba9008
ming-der wang <ming@Wangs-MacBook-Air.local>
2018-07-07 22:23:11 +0800

    init

--- components/Pixi.js
+++ components/Pixi.js
@@ -10,8 +10,26 @@ class PixiComponent extends React.Component {
   }
   
   componentDidMount() {
-    this.app = new PIXI.Application(window.innerWidth, window.innerHeight);
+    this.app = new PIXI.Application(window.innerWidth, window.innerHeight,
+   {
+  backgroundColor: 0x10bb99
+});
     this.gameCanvas.appendChild(this.app.view);
+
+//load an image and run the `setup` function when it's done
+PIXI.loader
+  .add("images/cat.png")
+  .load(setup);
+
+//This `setup` function will run when the image has loaded
+function setup() {
+
+  //Create the cat sprite
+  let cat = new PIXI.Sprite(PIXI.loader.resources["static/images/cat.png"].texture);
+
+  //Add the cat to the stage
+  app.stage.addChild(cat);
+}
     this.app.start();
   }
   
