533082d992c98b80d40c9575553e8825c1e70001
ming-der wang <ming@Wangs-MacBook-Air.local>
2018-07-09 12:20:24 +0800

    format code

--- components/Pixi.js
+++ components/Pixi.js
@@ -2,49 +2,49 @@ import React from 'react';
 import * as PIXI from "pixi.js";
 
 class PixiComponent extends React.Component {
-  gameCanvas = HTMLDivElement; 
+  gameCanvas = HTMLDivElement;
   app = PIXI.Application;
-  
+
   constructor() {
     super();
   }
-  
+
   componentDidMount() {
     this.app = new PIXI.Application(window.innerWidth, window.innerHeight,
-   {
-  backgroundColor: 0x10bb99
-});
+      {
+        backgroundColor: 0x10bb99
+      });
     this.gameCanvas.appendChild(this.app.view);
-// create a new Sprite from an image path
-var bunny = PIXI.Sprite.fromImage('static/images/cat.png')
+    // create a new Sprite from an image path
+    var bunny = PIXI.Sprite.fromImage('static/images/cat.png')
 
-// center the sprite's anchor point
-bunny.anchor.set(0.5);
+    // center the sprite's anchor point
+    bunny.anchor.set(0.5);
 
-// move the sprite to the center of the screen
-bunny.x = this.app.screen.width / 2;
-bunny.y = this.app.screen.height / 2;
+    // move the sprite to the center of the screen
+    bunny.x = this.app.screen.width / 2;
+    bunny.y = this.app.screen.height / 2;
 
-this.app.stage.addChild(bunny);
+    this.app.stage.addChild(bunny);
 
-// Listen for animate update
-this.app.ticker.add(function(delta) {
-    // just for fun, let's rotate mr rabbit a little
-    // delta is 1 if running at 100% performance
-    // creates frame-independent transformation
-    bunny.rotation += 0.1 * delta;
-});
+    // Listen for animate update
+    this.app.ticker.add(function (delta) {
+      // just for fun, let's rotate mr rabbit a little
+      // delta is 1 if running at 100% performance
+      // creates frame-independent transformation
+      bunny.rotation += 0.1 * delta;
+    });
     this.app.start();
   }
-  
+
   componentWillUnmount() {
     this.app.stop();
   }
-  
+
   render() {
     let component = this;
     return (
-      <div ref={(thisDiv) => {component.gameCanvas = thisDiv}} />
+      <div ref={(thisDiv) => { component.gameCanvas = thisDiv }} />
     );
   }
 }
