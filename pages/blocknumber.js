ca30a9821219817ff8577ea88375eace483a9f4b
ming-der wang <ming@Wangs-MacBook-Air.local>
2018-07-09 16:41:22 +0800

    fix account generated both on client and server

--- pages/blocknumber.js
+++ pages/blocknumber.js
@@ -1,28 +1,10 @@
-import React from 'react';
-import Web3 from 'web3';
+import dynamic from 'next/dynamic'
+const BlockNumber = dynamic(import('../components/Block'), {ssr: false})
 
-class Web3console extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-          accounts: ''
-        }
-      }
-
-    componentWillMount() {
-        console.log(web3)
-        var web3 = new Web3(Web3.givenProvider || 'http://localhost:8545');
-        var newAddress = web3.eth.accounts.create(web3.utils.randomHex(32));
-        this.setState({ accounts: newAddress.address });
-        console.log(newAddress)
-    }
-
-    render() {
-        var { accounts } = this.state
-        return (
-            <h1>{accounts}</h1>
-        );
-    }
+export default () => {
+  return (
+    <div>
+      <BlockNumber />
+    </div>
+  )
 }
-
-export default Web3console;
