acaca3d06019127e6b3d3cac44415eb8f442d928
Wang Ming-der <mingderwang@gmail.com>
2018-07-16 13:12:28 +0800

    add text ui

--- components/Pixi.js
+++ components/Pixi.js
@@ -34,6 +34,36 @@ class PixiComponent extends React.Component {
       // creates frame-independent transformation
       bunny.rotation += 0.1 * delta;
     });
+
+    var basicText = new PIXI.Text('Basic text in pixi');
+    basicText.x = 30;
+    basicText.y = 90;
+    
+    this.app.stage.addChild(basicText);
+    
+    var style = new PIXI.TextStyle({
+        fontFamily: 'Arial',
+        fontSize: 36,
+        fontStyle: 'italic',
+        fontWeight: 'bold',
+        fill: ['#ffffff', '#00ff99'], // gradient
+        stroke: '#4a1850',
+        strokeThickness: 5,
+        dropShadow: true,
+        dropShadowColor: '#000000',
+        dropShadowBlur: 4,
+        dropShadowAngle: Math.PI / 6,
+        dropShadowDistance: 6,
+        wordWrap: true,
+        wordWrapWidth: 440
+    });
+    
+    var richText = new PIXI.Text('Rich text with a lot of options and across multiple lines', style);
+    richText.x = 30;
+    richText.y = 180;
+    
+    this.app.stage.addChild(richText);
+    
     this.app.start();
   }
 
