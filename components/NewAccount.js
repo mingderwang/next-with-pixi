8ccd24554600537737c3688d829b2f9aa3ef0615
Ming-der Wang <mingderwang@gmail.com>
2018-10-10 17:22:21 +0800

    call works

--- components/NewAccount.js
+++ components/NewAccount.js
@@ -31,46 +31,57 @@ function createContract(web3, account) {
     }
   );
   console.log(Test);
-  Test.methods.myFunction().call()
-  .then(console.log);
+  Test.methods
+    .myFunction()
+    .call()
+    .then(console.log);
+
+  Test.methods
+    .myFunction()
+    .send({ from: account })
+    .on("receipt", function(receipt) {
+      console.log(receipt);
+    });
 }
 
 class Web3console extends React.Component {
   web3 = null;
-  codebase = "";
-
   constructor() {
     super();
     this.state = {
       accounts: "",
       privateKey: "",
-      showProviateKey: false
+      showProviateKey: false,
+      no_web3: true,
     };
     this.handleClick = this.handleClick.bind(this);
     this.handleShowPrivateKey = this.handleShowPrivateKey.bind(this);
   }
 
   componentWillMount() {
-    web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
-    this.web3 = web3;
-    var newAddress = web3.eth.accounts.create(web3.utils.randomHex(128));
-    this.setState({
-      accounts: newAddress.address,
-      privateKey: newAddress.privateKey
-    });
-    console.log("ming");
-    web3.eth.getAccounts().then(
-      accounts => {
-        createContract(this.web3, accounts[0]);
-      },
-      error => {
-        console.log(error);
-      }
-    );
+    var web3 = this.web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+    if (web3 !== undefined) {
+      this.setState({
+        no_web3: false
+      });
+      this.web3 = web3;
+      var newAddress = web3.eth.accounts.create(web3.utils.randomHex(128));
+      this.setState({
+        accounts: newAddress.address,
+        privateKey: newAddress.privateKey
+      });
+      web3.eth.getAccounts().then(
+        accounts => {
+          createContract(this.web3, accounts[0]);
+        },
+        error => {
+          console.log(error);
+        }
+      );
+    }
   }
 
   handleClick() {
-    console.log(this.web3);
     var newAddress = this.web3.eth.accounts.create(
       this.web3.utils.randomHex(128)
     );
@@ -85,22 +96,25 @@ class Web3console extends React.Component {
   }
 
   render() {
-    var { accounts, privateKey, showProviateKey } = this.state;
-    return (
-      <div>
-        <button onClick={this.handleClick}>Create a new account i </button>
-        <h1>Address:</h1>
-        <p />
-        <h2>{accounts}</h2>
-        <button onClick={this.handleShowPrivateKey}>
-          Show private key, or not i{" "}
-        </button>
-        <h1>Private Key:</h1>
-        <p />
-        <h3>{showProviateKey ? privateKey : "not to show"}</h3>
-      </div>
-    );
+    if (this.state.no_web3) {
+      return <h1>Please install Metamask (plugin)</h1>;
+    } else {
+      var { accounts, privateKey, showProviateKey } = this.state;
+      return (
+        <div>
+          <button onClick={this.handleClick}>Create a new account i </button>
+          <h1>Address:</h1>
+          <p />
+          <h2>{accounts}</h2>
+          <button onClick={this.handleShowPrivateKey}>
+            Show private key, or not i{" "}
+          </button>
+          <h1>Private Key:</h1>
+          <p />
+          <h3>{showProviateKey ? privateKey : "not to show"}</h3>
+        </div>
+      );
+    }
   }
 }
-
 export default Web3console;
